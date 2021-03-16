d8ffb4d519bff6bd62f0c448b47f7578112149bf
jonmpan <jonmpan@gmail.com>
2021-03-16 16:42:30 -0700

    added part 1 and part 2 for input-view-enhancements

---
+++ input-view-tutorial/code/GetContent.js
@@ -0,0 +1,31 @@
+const UTIL = require("./lib/util");
+const CONTENT = require("./content");
+
+// GetContent
+exports.function = function (searchTerm) {
+  //You can replace with a call to a web api - make sure you map api response to content model
+  var content = CONTENT
+  var chosenContent
+
+  // Get content from local content.js file 
+  // filter based on searchTerm (note that if you use a web API, then filtering can be done in the web API itself)
+  if (searchTerm) {
+    content = UTIL.findContent(content, searchTerm)
+  }
+
+  //pick a random content
+  if (content.length) {
+    var index = Math.floor(content.length * Math.random())
+    chosenContent = content[index]
+  }
+
+  // return content if exists, else null (No Result)
+  if (chosenContent) {
+    return {
+      text: chosenContent.text,
+      image: chosenContent.image
+    }
+  } else {
+    return null;
+  }
+}
---
+++ input-view-tutorial/code/lib/util.js
@@ -0,0 +1,18 @@
+exports.findContent = findContent
+
+function findContent (items, searchTerm) {
+  var matches = []
+  searchTerm = searchTerm.toLowerCase();
+
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].tags) {
+      for (var j = 0; j < items[i].tags.length; j++) {
+        if (searchTerm == items[i].tags[j].toLowerCase()) {
+          matches.push(items[i])
+          break
+        }
+      }
+    }
+  }
+  return matches
+}
